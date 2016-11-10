2ffe4ce2e44d5fa98dbcec52c6df09f8dab74361
Lizuca <Lizuca@Lizuca>
2016-11-11 00:01:01 +0200

    First Commit

--- JavaHomeworksNeagoeEliza/src/javahomeworksneagoeeliza/JavaHomeworksNeagoeEliza.java
+++ JavaHomeworksNeagoeEliza/src/javahomeworksneagoeeliza/JavaHomeworksNeagoeEliza.java
@@ -1,13 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Displaying a name inputed from keyboard
  */
 package javahomeworksneagoeeliza;
 
+import java.util.Scanner;
+
 /**
  *
- * @author gheorgheaurelpacurar
+ * @author Lizuca
  */
 public class JavaHomeworksNeagoeEliza {
 
@@ -15,7 +15,13 @@ public class JavaHomeworksNeagoeEliza {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("Introduceti numele si prenumele");
+       Scanner scanner;
+        scanner = new Scanner(System.in);
+       String numeprenume=scanner.nextLine();
+       System.out.println("Numele dumneavoastra este " + numeprenume);
+       
+             
+       
     }
-    
 }
