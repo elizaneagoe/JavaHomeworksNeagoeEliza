eb107eeebe60097a48f1b4fe2b9dac37a4512d62
Lizuca <Lizuca@Lizuca>
2016-11-11 21:26:49 +0200

    First Commit

--- JavaHomeworksNeagoeEliza/src/javahomeworksneagoeeliza/SinusCosinusNeagoeEliza.java
+++ JavaHomeworksNeagoeEliza/src/javahomeworksneagoeeliza/SinusCosinusNeagoeEliza.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Verifying the formula sin^x+cos^x=1 for a number inputed from keyboard
  */
 package javahomeworksneagoeeliza;
 
