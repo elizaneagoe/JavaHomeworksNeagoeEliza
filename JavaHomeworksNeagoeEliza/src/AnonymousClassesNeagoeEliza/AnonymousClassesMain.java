c3f24088af3e2f7affd2dc36fd7f5f46957a0b3e
Lizuca <Lizuca@Lizuca>
2016-12-01 23:36:26 +0200

    First Commit

---
+++ JavaHomeworksNeagoeEliza/src/AnonymousClassesNeagoeEliza/AnonymousClassesMain.java
@@ -0,0 +1,16 @@
+/*
+
+ */
+package AnonymousClassesNeagoeEliza;
+
+/**
+ *
+ * @author Lizuca
+ */
+public class AnonymousClassesMain {
+    public static void main(String... args) {
+HelloWorldAnonymousClasses myApp =
+new HelloWorldAnonymousClasses();
+myApp.sayHello();
+}
+}
